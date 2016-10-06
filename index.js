f045faef26f238bd570fdfca224b10a0914ca92d
Stephanie Lingwood <stephanielingwood@gmail.com>
2016-10-05 18:47:36 -0700

    Add memcached, postgres, rabbitMQ and SQLite (#3)
    * Add initialization and test for memcached
    
    * Use drydock/u14nodall:tip
    
    * Comment out neo4j to test other services
    
    * Adds postgres and couchdb
    
    * Add pg dependency
    
    * Use postgres 9.6
    
    * Create postgres database
    
    * Use default version of postgres
    
    * Add config and tests for rabbitMQ
    
    * comment out postgres
    
    * Add sqlite to yml
    
    * Add memcached, postgres, rabbitMQ and SQLite

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-//mysql
+var express = require("express");
+var app = express();
+
+// memcached
+var memjs = require('memjs');
+
+// mongo
+var mongoose = require("mongoose");
+
+// mysql
 var knex = require('knex');
 
-//neo4j
+// neo4j
 var neo4j = require('neo4j');
 
-//mongo
-var express = require("express"),
-    mongoose = require("mongoose"),
-    app = express();
+// postgres
+var knex = require('knex');
 
+// rabbitMQ
+var amqp = require('amqplib/callback_api');
 
-//DB initializations
+// initializations
+
+/* ================= memcached =================*/
+var memcachedClient = memjs.Client.create('127.0.0.1:11211', {
+  username: 'username',
+  password: 'password'
+});
+
+module.exports.memcachedClient = memcachedClient;
 
 /* ================= mongo =================*/
 mongoose.connect("mongodb://localhost/test", function (err) {
@@ -70,7 +87,62 @@ db_mysql.schema.hasTable('things').then(function (exists) {
 
 module.exports.db_mysql = db_mysql;
 
-
 /* ================= neo4j =================*/
 var db_neo4j = new neo4j.GraphDatabase('http://localhost:7474');
 module.exports.db_neo4j = db_neo4j;
+
+
+/* ================= postgres =================*/
+var db_postgres = knex.initialize({
+  client: 'pg',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'postgres',
+    password : '',
+    database : 'app_test'
+  }
+});
+
+module.exports.db_postgres = db_postgres;
+
+
+/* ================= rabbitMQ =================*/
+module.exports.rabbit_send = function (done) {
+  amqp.connect('amqp://localhost', function(err, connection) {});
+
+  amqp.connect('amqp://localhost', function(err, connection) {
+    connection.createChannel(function(err, ch) {});
+  });
+
+  amqp.connect('amqp://localhost',
+    function(err, connection) {
+      connection.createChannel(
+        function(err, ch) {
+          var q = 'hello';
+
+          ch.assertQueue(q, {durable: false});
+          ch.sendToQueue(q, new Buffer('Hello World!'));
+          console.log(" [x] Sent 'Hello World!'");
+          return done(null, 'sent message');
+        }
+      );
+    }
+  );
+};
+
+module.exports.rabbit_receive = function (done) {
+  amqp.connect('amqp://localhost',
+    function(err, conn) {
+      conn.createChannel(
+        function(err, ch) {
+          ch.consume('hello',
+            function(msg) {
+              console.log(" [x] Received %s", msg.content.toString());
+              return done(null, msg.content.toString());
+          },
+          {noAck: true});
+        }
+      );
+    }
+  );
+};
--- test.js
+++ test.js
@@ -1,34 +1,29 @@
+var memcachedClient = require('./index').memcachedClient;
 var db_mysql = require('./index.js').db_mysql;
 var db_neo4j = require('./index.js').db_neo4j;
+var db_postgres = require('./index.js').db_postgres;
+var rabbit_send = require('./index.js').rabbit_send;
+var rabbit_receive = require('./index.js').rabbit_receive;
 var chai = require("chai");
 var expect = chai.expect;
 var superagent = require("superagent");
 var should = require("should");
 
-/* ================= mysql =================*/
-describe('MySql Database', function () {
-  it('should create the things table', function () {
-    db_mysql.schema.hasTable('things').then(function (exists) {
-      expect(exists).to.equal(true);
-    });
-  });
-  it('should save a new name', function () {
-    db_mysql('things')
-      .insert({ name: 'Johnson' })
-      .exec(function (err) {
-        expect(err).to.equal(null);
-      });
-  });
-  it('should retrieve that name', function () {
-    db_mysql('things')
-      .where({ name: 'Johnson' })
-      .select('name')
-      .then(function (name) {
-        expect(name[0].name).to.equal('Johnson');
-      });
-  });
+/* ================= memcached =================*/
+describe('Memcached', function () {
+  it('sets, then gets a key/value pair',
+    function (done) {
+      memcachedClient.set('foo', 'bar');
+      memcachedClient.get('foo',
+        function (err, value, key) {
+          console.log(value.toString());
+          value.toString().should.equal('bar');
+          done();
+        }
+      );
+    }
+  );
 });
-
 
 /* ================= mongo =================*/
 describe("Index", function () {
@@ -65,6 +60,30 @@ describe("Persistence", function () {
   });
 });
 
+/* ================= mysql =================*/
+describe('MySql Database', function () {
+  it('should create the things table', function () {
+    db_mysql.schema.hasTable('things').then(function (exists) {
+      expect(exists).to.equal(true);
+    });
+  });
+  it('should save a new name', function () {
+    db_mysql('things')
+      .insert({ name: 'Johnson' })
+      .exec(function (err) {
+        expect(err).to.equal(null);
+      });
+  });
+  it('should retrieve that name', function () {
+    db_mysql('things')
+      .where({ name: 'Johnson' })
+      .select('name')
+      .then(function (name) {
+        expect(name[0].name).to.equal('Johnson');
+      });
+  });
+});
+
 /* ================= neo4j =================*/
 describe('Neo4j database', function () {
   it('should save a node', function () {
@@ -83,3 +102,67 @@ describe('Neo4j database', function () {
   });
 });
 
+/* ================= postgres =================*/
+describe('Postgres Database', function () {
+  this.timeout(3000);
+  beforeEach(function (done) {
+    setTimeout(function () {
+      done();
+    }, 1000);
+  });
+  it('should create a table', function (done) {
+    db_postgres.schema.hasTable('things').then(function (exists) {
+      if (!exists) {
+        db_postgres.schema.createTable('things', function (table) {
+          table.string('name');
+        }).then(function () {
+          done();
+        });
+      } else {
+        done();
+      }
+    });
+  });
+  it('should save a new name', function (done) {
+    db_postgres('things')
+      .insert({ name: 'Johnson' })
+      .exec(function (err) {
+        expect(err).to.equal(null);
+        done();
+      });
+  });
+  it('should retrieve that name', function (done) {
+    db_postgres('things')
+      .select()
+      .then(function (docs) {
+        expect(docs[0].name).to.equal('Johnson');
+        done();
+      });
+  });
+});
+
+/* ================= rabbitMQ =================*/
+describe('RabbitMQ',
+  function () {
+    it('should send a message',
+      function (done) {
+        rabbit_send(
+          function(err, sentMessage) {
+            expect(sentMessage).to.equal('sent message');
+            done();
+          }
+        );
+      }
+    );
+    it('should receive a message',
+      function (done) {
+        rabbit_receive(
+          function(err, receivedMessage) {
+            expect(receivedMessage).to.equal('Hello World!');
+            done();
+          }
+        );
+      }
+    );
+  }
+);
